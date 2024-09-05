d0d1a22358acadb3f8be731ad43dc768be4521c5
kishu98 <kishujain.1998@gmail.com>
2024-09-05 20:50:14 +0530

    city

--- src/components/Weather/getLocation.js
+++ src/components/Weather/getLocation.js
@@ -1,8 +1,9 @@
 export async function getLocation() {
 
     const API_KEY = import.meta.env.VITE_API_KEY;
+    const CITY = import.meta.env.VITE_CITY;
    
-    const res = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=bangalore&limit=3&appid=${API_KEY}`);
+    const res = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${CITY}&limit=3&appid=${API_KEY}`);
 
     if(res.ok) {
         let data = await res.json();
