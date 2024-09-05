408a555c825eaaaf0623d8e29764923f9967318a
kishu98 <kishujain.1998@gmail.com>
2024-09-05 20:03:33 +0530

    minor changes

--- src/components/Forecast/Forecast.jsx
+++ src/components/Forecast/Forecast.jsx
@@ -1,12 +1,12 @@
 import { useEffect, useState } from "react";
 import { getLocation } from "../Weather/getLocation";
+import "./Forecast.css";
 
 export function Forecast() {
   const API_KEY = import.meta.env.VITE_API_KEY;
 
-  const [iconLinks, setIconLinks] = useState([]);
-  const [temps, setTemps] = useState([]);
   const [forecast, setForecast] = useState([]);
+  const daysOfWeek = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
 
   useEffect(() => {
     async function getForecast() {
@@ -30,15 +30,21 @@ export function Forecast() {
   }, []);
 
   return (
-    <>
-      <ul>
+    <div className='container'>
+      <ul className='forecast-list'>
         {forecast.map((forecast) => (
           <li>
-            <img src={`https://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png`}></img>
-            <p>{forecast.main.temp}</p>
+            <div className='dayForecast'>
+              <p className='day'>{daysOfWeek[new Date(forecast.dt * 1000).getDay()]}</p>
+              <img
+                className='icon'
+                src={`https://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png`}
+              ></img>
+              <p className='temperature'>{forecast.main.temp.toFixed(0)}</p>
+            </div>
           </li>
         ))}
       </ul>
-    </>
+    </div>
   );
 }
--- src/components/Weather/Weather.jsx
+++ src/components/Weather/Weather.jsx
@@ -5,61 +5,61 @@ import { getWeather } from "./getWeather";
 import { Forecast } from "../Forecast/Forecast";
 
 export function Weather() {
-    const [weather, setWeather] = useState("");
-    const [location, setLocation] = useState("");
-    const [temp, setTemp] = useState(0);
-    const [isCelsius, setIsCelsius] = useState(true);
-    const [icon, setIcon] = useState("");
+  const [weather, setWeather] = useState("");
+  const [location, setLocation] = useState("");
+  const [temp, setTemp] = useState(0);
+  const [isCelsius, setIsCelsius] = useState(true);
+  const [icon, setIcon] = useState("");
 
-    useEffect(() => {
-        async function getData() {
-            // Getting  location details
-            const coor = await getLocation();
-            // console.log(coor);
-            // Getting current weather details
-            const data = await getWeather(coor);
-            // console.log(data);
+  useEffect(() => {
+    async function getData() {
+      // Getting  location details
+      const coor = await getLocation();
+      // console.log(coor);
+      // Getting current weather details
+      const data = await getWeather(coor);
+      // console.log(data);
 
-            let loc = `${coor.name}, ${coor.country}`;
-            setLocation(loc);
-            let iconLink = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
-            setIcon(iconLink);
-            setTemp(data.main.temp.toFixed(0));
-            setWeather(data.weather[0].description.toUpperCase());
-        }
-
-        getData();
-
-        const interval = setInterval(() => {
-            getData();
-        }, 300000);
-
-        return () => clearInterval(interval);
-    }, []);
-
-    function handleUnitChange() {
-        let newTemp = temp;
-        if (isCelsius == true) {
-            newTemp = (newTemp * 9) / 5 + 32;
-            newTemp = newTemp.toFixed(0);
-        } else {
-            newTemp = ((newTemp - 32) * 5) / 9;
-            newTemp = newTemp.toFixed(0);
-        }
-        setTemp(newTemp);
-        setIsCelsius(!isCelsius);
+      let loc = `${coor.name}, ${coor.country}`;
+      setLocation(loc);
+      let iconLink = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+      setIcon(iconLink);
+      setTemp(data.main.temp.toFixed(0));
+      setWeather(data.weather[0].description.toUpperCase());
     }
 
-    return (
-        <div className='currentWeather'>
-            <img className='weatherIcon' src={icon}></img>
-            <p className='weather'>{weather}</p>
-            <p className='location'>{location.toUpperCase()}</p>
-            <p className='temp'>
-                {temp}° {isCelsius ? "C" : "F"}
-            </p>
-            {/* <button className="unit" onClick={handleUnitChange}>Change unit</button> */}
-            <Forecast />
-        </div>
-    );
+    getData();
+
+    const interval = setInterval(() => {
+      getData();
+    }, 300000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  function handleUnitChange() {
+    let newTemp = temp;
+    if (isCelsius == true) {
+      newTemp = (newTemp * 9) / 5 + 32;
+      newTemp = newTemp.toFixed(0);
+    } else {
+      newTemp = ((newTemp - 32) * 5) / 9;
+      newTemp = newTemp.toFixed(0);
+    }
+    setTemp(newTemp);
+    setIsCelsius(!isCelsius);
+  }
+
+  return (
+    <div className='currentWeather'>
+      <img className='weatherIcon' src={icon}></img>
+      <p className='weather'>{weather}</p>
+      <p className='location'>{location.toUpperCase()}</p>
+      <p className='temp'>
+        {temp}° {isCelsius ? "C" : "F"}
+      </p>
+      {/* <button className="unit" onClick={handleUnitChange}>Change unit</button> */}
+      <Forecast />
+    </div>
+  );
 }
